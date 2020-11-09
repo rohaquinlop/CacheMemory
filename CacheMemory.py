49a8de55f1346a572fa2b3134ffa6abf8c13afd8
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-11-08 23:58:08 -0500

    Created read and write functions
    added cntHits and cntMiss

--- CacheMemory.py
+++ CacheMemory.py
@@ -1,5 +1,7 @@
 from math import log2
-from blockClass import Block as block
+from collections import deque
+from blockClass import Block
+from AddressClass import Address
 
 class CacheMemory:
   def __init__(self, cntBlocks, blocksSize, cntVias):
@@ -7,6 +9,19 @@ class CacheMemory:
     self.blocksSize = blocksSize
     self.cntVias = cntVias
     self.cntSets = cntBlocks//cntVias
+    self.cntHits = 0
+    self.cntMiss = 0
+    self.cola = deque() #Unicamente la etiqueta y el indice -> [tag, idx]
 
     ##Inicialización de la memoria caché con todos los espacios vacíos
-    self.content = [ [ block("0", "0", "", "") for _ in range(cntVias) ] for _ in range(self.cntSets)  ]
+    self.content = [ [ Block("0", "0", "", "") for _ in range(cntVias) ] for _ in range(self.cntSets)  ]
+  
+  ## Metodo de escritura -> Leer de la memoria caché con un address
+  def read(self, stringAddress):
+    ##Recibe un objeto de la clase address
+    address = Address(stringAddress)
+
+  ## Metodo de lectura -> Escribir en la memoria caché con un address
+  def write(self, stringAddress):
+    ##Recibe un objeto de la clase address
+    address = Address(stringAddress)
