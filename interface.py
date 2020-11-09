fdb197c69810fd72f1090e829e50464ae890dc5e
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-11-09 14:41:21 -0500

    added file writing

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import random
 from blockClass import Block
+from AddressClass import Address
 
 def interfaz(cache, mainMemo):
   Exit = False
@@ -32,6 +33,10 @@ def interfaz(cache, mainMemo):
 
         addressRandom = random.randint(0, 2047)
         addressString = "{0:011b}".format(addressRandom)
+        address = Address(addressString)
+
+        print("Address: |{}|{}|{}|\n".format(address.tag, address.index, address.offset))
+        print("- - - - -")
 
         if opt == "1":
           cache.read(addressString, mainMemo)
@@ -39,3 +44,33 @@ def interfaz(cache, mainMemo):
           cache.write(addressString, mainMemo)
     else:
       print("Opcion incorrecta")
+    
+    for c in cache.content:
+      print("|{}|{}|".format(c[0].dirty+" "+c[0].tag + " "+ c[0].data, c[1].dirty+" "+c[1].tag + " " + c[1].data))
+    
+    ##Guardar los datos en txt
+    cacheFile = open("cacheOutput.txt", "w")
+
+    for c in cache.content:
+      line = "|{}|{}|\n".format(c[0].dirty+" "+c[0].tag + " "+ c[0].data, c[1].dirty+" "+c[1].tag + " " + c[1].data)
+      cacheFile.write(line)
+    
+    totalRates = cache.cntHits+cache.cntMiss
+    if totalRates > 0:
+      line = "- - - - -\n"
+      totalRates = cache.cntHits+cache.cntMiss
+      hitsRate = (cache.cntHits*100)/totalRates
+      line += "Hits rate: {:.2f}% | Miss rate: {:.2f}%\n".format( hitsRate, 100-hitsRate)
+      line += "- - - - -\n"
+      cacheFile.write(line)
+
+    cacheFile.close()
+
+    ##Guardar los datos en txt
+    RAMoutput = open("RAMoutput.txt", "w")
+
+    for c in mainMemo.content:
+      line = "{}\n".format(c)
+      RAMoutput.write(line)
+
+    RAMoutput.close()
